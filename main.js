94c74ab41890e6dbf0cc3904eb47c58b89fa5b08
Urbautz <40495078+Urbautz@users.noreply.github.com>
2022-10-20 16:08:27 +0200

    Make Nails, Update Nails-Display

---
+++ main.js
@@ -0,0 +1,40 @@
+// Settings object
+let settings = {
+  Language:"EN/US"
+};
+
+let limits = [
+  {LimitLow: 25n, Show:'press10'}, 
+  {LimitLow: 1000n, Show:'xzy'}
+]
+
+let save = {
+  Nails: 0n,
+  NailsPerTick: 0n,
+  StorageGarage: 1n,
+  StorageWarehouseSmall: 0n,
+  StorageWarehouseMedium: 0n
+};
+
+function format(value){
+  return value;
+}
+
+function load() {
+  document.getElementById('NailsTotal').innerHTML = 0; 
+  updateVisibility();
+}
+
+function makeNail(count=0) {
+  save.Nails += BigInt(count) + save.NailsPerTick;
+  document.getElementById('NailsTotal').innerHTML = format(save.Nails);
+  updateVisibility();
+}
+
+function updateVisibility(){
+  limits.forEach(limit => {
+    if(limit.LimitLow < save.Nails ) {
+      document.getElementById(limit.Show).style.visibility = "visible";
+    }
+  });
+}
